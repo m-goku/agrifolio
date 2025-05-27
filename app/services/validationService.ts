4bfe617185cb61a7659b3012e54a90dfc9d7b6d7
goku-ts <hgb.mega@gmail.com>
2025-05-27 01:45:17 +0000

    1

--- app/services/validationService.ts
+++ app/services/validationService.ts
@@ -1,38 +1,31 @@
-import Joi from "joi"
+import Joi from "joi";
 
-import { UserLoginTypes, UserRegisterTypes } from "../types/types"
-
-
-export const validateRegister = (body: UserRegisterTypes) => {
-  const validationschema = Joi.object<UserRegisterTypes>({
+export const validateRegister = (body: any) => {
+  const validationschema = Joi.object({
     email: Joi.string().email().required(),
     businessName: Joi.string().required().min(5).max(30),
     password: Joi.string().required().min(6).max(25),
-  })
+  });
 
-  return validationschema.validate(body)
-}
+  return validationschema.validate(body);
+};
 
-export const validateLogin = (body: UserLoginTypes) => {
-  const validationschema = Joi.object<UserLoginTypes>({
+export const validateLogin = (body: any) => {
+  const validationschema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().required().min(6).max(25)
+    password: Joi.string().required().min(6).max(25),
     // .pattern(new RegExp("^[a-zA-Z0-9@]{3,30}$"))
-  })
+  });
 
-  return validationschema.validate(body)
-}
+  return validationschema.validate(body);
+};
 
-
-
-export const validatePost = (body) => {
+export const validatePost = (body: any) => {
   const validationschema = Joi.object({
     title: Joi.string().required(),
     content: Joi.string().required(),
     description: Joi.string().required(),
-  })
+  });
 
-  return validationschema.validate(body)
-}
-
-
+  return validationschema.validate(body);
+};
